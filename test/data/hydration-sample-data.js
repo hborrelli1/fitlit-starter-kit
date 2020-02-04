46a60e924a86048318d7572fbba740b7777ee5ad
Harry Borrelli <hborrelli@outlook.com>
2020-02-04 16:51:07 -0700

    setup test folders & files, sample data structures

---
+++ test/data/hydration-sample-data.js
@@ -0,0 +1,36 @@
+const hydrationData = [
+  {
+    "userID": 1,
+    "date": "2019/07/18",
+    "numOunces": 37
+  },
+  {
+    "userID": 1,
+    "date": "2019/07/25",
+    "numOunces": 43
+  },
+  {
+    "userID": 2,
+    "date": "2019/07/01",
+    "numOunces": 75
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/16",
+    "numOunces": 47
+  },
+  {
+    "userID": 4,
+    "date": "2019/06/15",
+    "numOunces": 85
+  },
+  {
+    "userID": 5,
+    "date": "2019/06/10",
+    "numOunces": 42
+  }
+];
+
+if (typeof module !== 'undefined') {
+  module.exports = hydrationData;
+}
---
+++ test/hydration-test.js
@@ -0,0 +1,39 @@
+const chai = require("chai");
+const expect = require('chai').expect;
+const Hydration = require('../classes/hydration');
+const hydrationData = require('./data/hydration-sample-data');
+
+describe('Hydration', () => {
+  let hydration;
+
+  beforeEach(() => {
+    hydration = new Hydration(hydrationData);
+  });
+
+  describe('class initialization', () => {
+    it('should be able to instantiate an instance of the hydration class', () => {
+      expect(hydration).to.be.an.instanceof(Hydration);
+    });
+
+    it('should be able to take in hydration events', () => {
+      expect(hydration.hydrationEvents.length).to.equal(6);
+    });
+  });
+
+  describe('testing methods', () => {
+    it('should be able to get users average consumed water of all time.', () => {
+      expect(hydration.getAvgConsumedAllTime(1)).to.equal(80);
+    });
+
+    it('should be able to get users total amount of water consumed by date', () => {
+      expect(hydration.getTotalConsumedByDate(3, '2019/06/16')).to.equal(47);
+    });
+
+    it('should be able to get users weekly water consumption', () => {
+      expect(hydration.getWeeklyConsumption(1, ['2019/07/18', '2019/07/25'])).to.deep.equal([{date: '2019/07/18', numOfOunces: 37},{date: '2019/07/25', numOfOunces: 43}]);
+    })
+  });
+
+
+
+})
