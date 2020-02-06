084ae9703d46572280ca6cdc7071254694c84f1c
Mandy Alford <mandyalford@gmail.com>
2020-02-05 19:15:02 -0700

    Refactor getFriends

--- src/scripts.js
+++ src/scripts.js
@@ -14,27 +14,17 @@ console.log(randNum);
 console.log(currentUser);
 
 let names = getFriends(currentUser.friends);
-// console.log(currentUserFriends);
 
 function getFriends(idArray) {
   let friendsNames = idArray.map(function(id) {
-    // const matchingUserData = userData.find(function(userInfo) {
-    //   return userInfo.id === id
-    // })
-    // return matchingUserData.name
     return userData.find(function(userInfo) {
       return userInfo.id === id;
     }).name;
   })
-  console.log(friendsNames.join(', '));
   return friendsNames.join(', ');
 }
 
-// creat array of currentUsers friends (objects)
-// Loop through each object to push name (li with name) to usersFriends below.
-// usersFriends then gets pushed to the friends element.
 
-// Populate user information in dom
 userName.innerText = currentUser.name;
 address.innerText = currentUser.address;
 email.innerText = currentUser.email;
