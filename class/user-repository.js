1a50c9d2c49868b3d33105de6384c2c1a65a518d
Mandy Alford <mandyalford@gmail.com>
2020-02-06 16:19:30 -0700

    Move getFrinds function into user respository instead of scripts

--- class/user-repository.js
+++ class/user-repository.js
@@ -15,6 +15,14 @@ class UserRepository {
     }, 0)
     return sumOfAllStepGoals / this.data.length;
   }
+  getFriends(idArray) {
+    let friendsNames = idArray.map(function(id) {
+      return this.data.find(function(userInfo) {
+        return userInfo.id === id;
+      }).name;
+    }.bind(this))
+    return friendsNames.join(', ');
+  };
 }
 
 if (typeof module !== 'undefined') {
