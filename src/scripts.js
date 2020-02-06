828b079509082c58b9497774439591edf70fe5d3
Harry Borrelli <hborrelli@outlook.com>
2020-02-05 20:37:10 -0700

    Add amount of water drank today to DOM.

--- src/scripts.js
+++ src/scripts.js
@@ -4,15 +4,29 @@ const email = document.getElementById('email');
 const strideLenth = document.getElementById('strideLenth');
 const dailyStepGoal = document.getElementById('dailyStepGoal');
 const friends = document.getElementById('friends');
+const waterComsumptionToday = document.getElementById('waterComsumptionToday');
 
 let userRepo = new UserRepository(userData);
-
 let randNum = Math.floor(Math.random() * 50) + 1;
-
 let currentUser = userRepo.getUserInfo(randNum);
-console.log(randNum);
-console.log(currentUser);
+let hydrationDataset = new Hydration(hydrationData);
 
+let todaysDate = '2019/09/22';
+/*
+page loads
+userID generated
+userRepo created
+hydration for user is fetched
+
+*/
+let userWaterConsumption = hydrationDataset.getTotalConsumedByDate(randNum, todaysDate);
+console.log(userWaterConsumption);
+
+waterComsumptionToday.innerHTML = userWaterConsumption;
+
+// function getHydrationInfo(userID) {
+//
+// }
 // creat array of currentUsers friends (objects)
 // Loop through each object to push name (li with name) to usersFriends below.
 // usersFriends then gets pushed to the friends element.
@@ -23,6 +37,11 @@ address.innerText = currentUser.address;
 email.innerText = currentUser.email;
 strideLength.innerText = currentUser.strideLength;
 dailyStepGoal.innerText = currentUser.dailyStepGoal;
+
+// Populate Hydration information
+// waterComsumptionToday.innerText =
+
+
 
 // let usersFriends = '';
 
