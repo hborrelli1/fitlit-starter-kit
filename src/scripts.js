0857d800d27d0a6feec917533c96ba9fac47608c
Mandy Alford <mandyalford@gmail.com>
2020-02-06 17:37:45 -0700

    Remove comments and debuggers

--- src/scripts.js
+++ src/scripts.js
@@ -16,7 +16,7 @@ let randNum = Math.floor(Math.random() * 50) + 1;
 let currentUser = new User(userRepo.getUserInfo(randNum));
 
 let names = userRepo.getFriends(currentUser.friends);
-// debugger
+
 
 function addFriendNames(){
   let friends = names.map(function(name){
@@ -31,19 +31,5 @@ address.innerText = currentUser.address;
 email.innerText = currentUser.email;
 strideLength.innerText = currentUser.strideLength;
 dailyStepGoal.innerText = currentUser.dailyStepGoal;
-// friends.innerText = names;
-debugger
 friends.innerHTML = addFriendNames(names);
 totalStepGoal.innerText = allStepGoals;
-
-// let usersFriends = '';
-
-// currentUser.friends.forEach(friend => usersFriends += friend.friends);
-// console.log(usersFriends);
-
-// friends.insertAdjacentHTML('beforeend', usersFriends);
-
-
-// Grab user from userRepository by id (randNum)
-
-// Instantiate as currentUser
